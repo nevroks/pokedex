4569058902e4ba572ea664fcdda8ff68d7867940
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-14 22:21:17 +0500

    added navigation and moved to another directory

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,7 +3,8 @@
         <div class="header-content">
             <v-icon name="MdCatchingpokemon" scale=2  />
             <nav class="header__navigation">
-
+                <router-link to="/">Homepage</router-link>
+                <router-link to="/profile">Profile</router-link>
             </nav>
         </div>
 
@@ -12,15 +13,24 @@
 
 <script>
 
+import ProfilePage from "../../pages/ProfilePage.vue";
+
 export default {
+    components: {ProfilePage}
 
 }
 </script>
 
 <style scoped>
 .header{
+    display: flex;
+    justify-content: space-between;
     padding-top: 20px;
     padding-bottom: 20px;
     background-color: var(--blue-color);
+}
+.header__navigation{
+    display: flex;
+    column-gap: 30px;
 }
 </style>
