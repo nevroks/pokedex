15fd955345db0ef0be9d7cd1d76d7fd4897c14c0
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-14 22:22:40 +0500

    added custom button and custom input

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="btn">
+      <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+  name:"Button"
+}
+</script>
+
+<style scoped>
+.btn{
+
+}
+</style>
---
+++ src/components/UI/Input.vue
@@ -0,0 +1,26 @@
+<template>
+    <input :value="modelValue" @input="updateValue" class="input">
+</template>
+
+<script>
+export default {
+    name:"Input",
+    props:{
+        modelValue:String | Number
+    },
+    methods:{
+        updateValue(event){
+        this.$emit('update:modelValue',event.target.value)
+        }
+    }}
+</script>
+
+<style scoped>
+.input{
+    border: 1px solid black;
+    background-color: var(--main-font-color);
+    border-radius: 10px;
+    padding: 5px 10px;
+    color: white;
+}
+</style>
